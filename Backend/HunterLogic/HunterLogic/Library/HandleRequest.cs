078b9a4c123acac9604bc32042f14125d919026e
MaeleGA <maelegaboinewe@gmail.com>
2024-04-06 17:28:21 +0200

    Initial Class diagram and Backend code for Realtime currency data

---
+++ Backend/HunterLogic/HunterLogic/Library/HandleRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HunterLogic.Library
+{
+	public class HandleRequest
+	{
+		public HandleRequest()
+		{
+            // Live Rates
+            string List1 = "https://fxmarketapi.com/apilive?api_key=api_key";
+            // Available Currencies
+            string List2 = "https://fxmarketapi.com/apicurrencies?api_key=api_key";
+            // Historical Rates
+            string List3 = "https://fxmarketapi.com/apihistorical?api_key=api_key&date=2018-05-02";
+            // Currency Conversion
+            string List4 = "https://fxmarketapi.com/apiconvert?api_key=api_key&from=EUR&to=SEK&amount=1000";
+
+            // Time-Series
+            string List5 = "https://fxmarketapi.com/apitimeseries?api_key=api_key&currency=EURUSD,GBPUSD&start_date=2018-07-02&end_date=2018-09-03&format=ohlc";
+        }
+	}
+}
+
---
+++ Backend/HunterLogic/HunterLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HunterLogic;
+using System.Net;
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Sending request ...");
+        CheckThis();
+        Console.ReadKey();
+    }
+
+    private static async void CheckThis()
+    {
+        HttpClient client = new HttpClient();
+        string APIString = "https://fxmarketapi.com/apilive?api_key=ujuSghNweo0JX72IvPmZ";
+        string Response = await client.GetStringAsync(APIString);
+        Console.WriteLine(Response);
+    }
+}
+
